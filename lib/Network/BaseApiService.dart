a62d5b159d0b9c64d8042b0fbf3d12c11b6dee77
OussemaZaier <medoussemazaier@gmail.com>
2023-07-20 15:20:40 +0100

    api put

--- lib/Network/BaseApiService.dart
+++ lib/Network/BaseApiService.dart
@@ -2,4 +2,5 @@ abstract class BaseApiService {
   Future<dynamic> get(String url);
   Future<dynamic> getById(String url, String id);
   Future<dynamic> post(String url, Map<String, dynamic> body);
+  Future<dynamic> update(String url, Map<String, dynamic> body);
 }
--- lib/Network/NetworkApiService.dart
+++ lib/Network/NetworkApiService.dart
@@ -102,4 +102,35 @@ class NetworkApiService<T> extends BaseApiService {
     }
     return apiResponse.toJson();
   }
+
+  @override
+  Future update(String url, Map<String, dynamic> body) async {
+    var apiResponse = ApiResponse<T>.loading();
+
+    try {
+      final response = await dio.put(url, data: body);
+
+      // Handle the response
+      if (response.statusCode == 200) {
+        // Request successful
+        if (kDebugMode) {
+          print(response.data);
+        }
+        apiResponse = ApiResponse.completed(response.data);
+      } else {
+        // Request failed
+        if (kDebugMode) {
+          print('Request failed with status code: ${response.statusCode}');
+        }
+        apiResponse = ApiResponse.error(response.data);
+      }
+    } catch (error) {
+      // Exception occurred during the request
+      if (kDebugMode) {
+        print('An error occurred: $error');
+      }
+      apiResponse = ApiResponse.error({"error": error});
+    }
+    return apiResponse.toJson();
+  }
 }
