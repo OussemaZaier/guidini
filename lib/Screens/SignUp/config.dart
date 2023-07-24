ada69d332d17852a14152b950589d2a09eaaba8c
SEEF <seifeddine.gherir@etudiant-fst.utm.tn>
2023-07-24 10:14:41 +0100

    Fetch ads from DB

--- lib/Screens/SignUp/config.dart
+++ lib/Screens/SignUp/config.dart
@@ -2,6 +2,7 @@ final url = 'http://10.72.0.126:3000';
 final registration = url + '/auth/signup';
 final login = url + '/auth/login';
 final getUsers = url + '/user';
+final ads = url + '/ad';
 
 String getUser(String id) {
   return url + '/user/' + id;
